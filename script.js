3c093960b4a65bd37f449dd119e8c8e387c5e48a
Tuhin Ghosh <66065760+Tuhin1904@users.noreply.github.com>
2025-01-06 17:04:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+document.getElementById("userForm").addEventListener("submit", function(event){
+	event.preventDefault();
+
+	const name = document.getElementById("name").value.trim();
+	const age = document.getElementById("age").value.trim();
+
+	if(!name || !age){
+		alert("Both fields are required!");
+		return;
+	}
+
+	new Promise((resolve, reject) =>{
+		setTimeout(() => {
+			if(parseInt(age) > 18){
+				resolve(`Welcome, ${name}. You can vote.`)
+			}else{
+				reject(`Oh sorry ${name}. You aren't old enough.`)
+			}
+			
+		}, 4000)
+	})
+	.then((msg) => {
+		alert(msg)
+	})
+	.catch((err) => {
+		alert(err)
+	});
+})
